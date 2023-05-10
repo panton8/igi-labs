5f6b125f6e017fe5fd36e265286078d608b8d738
Anton Padvalnikau <podvalnikov0@gmail.com>
2023-05-10 09:34:04 +0300

    Add methods for json serialization

--- lab3/serializers/serializer_json.py
+++ lab3/serializers/serializer_json.py
@@ -1,4 +1,8 @@
+import re
+import regex
 from lab3.serializers.serializer import Serializer
+from lab3.serializers.packing_tool import PackingTool
+from lab3.serializers.consts import nonetype
 
 
 class Json(Serializer):
@@ -24,4 +28,91 @@ class Json(Serializer):
 
     VALUE_PATTERN = fr"\s*({ELEMENTARY_TYPES_PATTERN}|" + \
                     fr"{ARRAY_PATTERN}|{OBJECT_PATTERN})\s*"
+
+    def dumps(self, obj) -> str:
+        obj = PackingTool.to_dict(obj)
+        return self.__dumps_from_dict(obj)
+
+    def __dumps_from_dict(self, obj) -> str:
+        if type(obj) in (int, float):
+            return str(obj)
+
+        if type(obj) is bool:
+            return self.TRUE_LITERAL if obj else self.FALSE_LITERAL
+
+        if type(obj) is str:
+            return '"' + self.__mask_quotes(obj) + '"'
+
+        if type(obj) is nonetype:
+            return self.NULL_LITERAL
+
+        if type(obj) is list:
+            return '[' + ", ".join([self.__dumps_from_dict(item) for item in obj]) + ']'
+
+        if type(obj) is dict:
+            return '{' + ", ".join([f"{self.__dumps_from_dict(item[0])}: "
+                                    f"{self.__dumps_from_dict(item[1])}" for item in obj.items()]) + '}'
+        else:
+            raise ValueError
+
+    def loads(self, string: str):
+        obj = self.__loads_to_dict(string)
+        return PackingTool.from_dict(obj)
+
+    def __loads_to_dict(self, string: str):
+        string = string.strip()
+
+        # Int
+        match = re.fullmatch(self.INT_PATTERN, string)
+        if match:
+            return int(match.group(0))
+
+        # Float
+        match = re.fullmatch(self.FLOAT_PATTERN, string)
+        if match:
+            return float(match.group(0))
+
+        # Bool
+        match = re.fullmatch(self.BOOL_PATTERN, string)
+        if match:
+            return match.group(0) == self.TRUE_LITERAL
+
+        # Str
+        match = re.fullmatch(self.STRING_PATTERN, string)
+        if match:
+            ans = match.group(0)
+            ans = self.__unmask_quotes(ans)
+            return ans[1:-1]
+
+        # None
+        match = re.fullmatch(self.NULL_PATTERN, string)
+        if match:
+            return None
+
+        # List
+        if string[0] == '[' and string[-1] == ']':
+            string = string[1:-1]
+            matches = regex.findall(self.VALUE_PATTERN, string)
+            return [self.__loads_to_dict(match[0]) for match in matches]
+
+        # Dict
+        if string[0] == '{' and string[-1] == '}':
+            string = string[1:-1]
+            matches = regex.findall(self.VALUE_PATTERN, string)
+
+            # Variable matches will store key-value pairs in one row. Elements with
+            # even indexes are keys, those with odd indexes are values.
+            return {self.__loads_to_dict(matches[i][0]):
+                    self.__loads_to_dict(matches[i + 1][0]) for i in range(0, len(matches), 2)}
+
+        else:
+            raise ValueError
+
+    @staticmethod
+    def __mask_quotes(string: str) -> str:
+        return string.replace('\\', "\\\\").replace('"', r"\"").replace("'", r"\'")
+
+    @staticmethod
+    def __unmask_quotes(string: str) -> str:
+        return string.replace('\\\\', "\\").replace(r"\"", '"').replace(r"\'", "'")
 
