155b6862f57cca43225e18445f6b26b86377007a
Anton Padvalnikau <podvalnikov0@gmail.com>
2023-06-04 14:33:31 +0300

    Add form for tickets

--- lab4/mysite/cinema/forms.py
+++ lab4/mysite/cinema/forms.py
@@ -1,16 +1,19 @@
 from django import forms
-from .models import Film
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
-
-
-class SignUpForm(forms.Form):
-    user_name = forms.CharField(min_length=5, max_length=20)
-    email = forms.EmailField()
-    password = forms.CharField(widget=forms.PasswordInput())
+from .models import *
 
 
 class FilmForm(forms.ModelForm):
     class Meta:
         model = Film
         fields = "__all__"
+
+
+class TicketPostForm(forms.Form):
+    class Meta:
+        model = Session
+        fields = "__all__"
+        widgets = {
+            "Film": forms.Form
+        }
+    session = forms.ModelChoiceField(queryset=Session.objects.all())
+    seats = forms.IntegerField(min_value=1, max_value=5)
