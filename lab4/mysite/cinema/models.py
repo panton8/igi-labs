39ab42f5a38914d3c277674797826a2a2f08f724
Anton Padvalnikau <podvalnikov0@gmail.com>
2023-06-05 02:07:00 +0300

    Update models

--- lab4/mysite/cinema/models.py
+++ lab4/mysite/cinema/models.py
@@ -1,6 +1,4 @@
-from audioop import reverse
-
-from django.contrib.auth.models import AbstractUser
+from django.urls import reverse
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
@@ -38,6 +36,9 @@ class Genre(models.Model):
 
     def __str__(self):
         return f'{self.genre}'
+
+    def get_absolute_url(self):
+        return reverse('genre', kwargs={'genre_id': self.pk})
 
 
 class Country(models.Model):
@@ -94,31 +95,27 @@ class Film(models.Model):
     rating = models.FloatField(validators=[MinValueValidator(1.0), MaxValueValidator(10.0)], help_text="from 1.0 to 10.0")
 
     def __str__(self):
-        return f'Film info:\n' \
-               f'Title: {self.name}\n' \
-               f'Country of production: {self.country}\n' \
-               f'Genre: {self.genre}\n' \
-               f'Running Time: {self.duration}\n' \
-               f'Budget: {self.budget}\n' \
-               f'Rating: {self.rating}'
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('film', kwargs={'film_id': self.pk})
 
 
 class Hall(models.Model):
     name = models.CharField(max_length=30)
     photo = models.ImageField(upload_to="photos/halls")
     capacity = models.IntegerField(validators=[MinValueValidator(50), MaxValueValidator(800)])
-    available_seats = models.IntegerField(validators=[MinValueValidator(0)])
     cashiers = models.ManyToManyField(Cashier, through="Session")
 
     def __str__(self):
-        return f"Hall Info:\n" \
-               f"Name: {self.name}\n" \
+        return f"{self.name[-3:]}\n" \
                f"Capacity: {self.capacity}"
 
 
 class Session(models.Model):
     film = models.ForeignKey(Film, on_delete=models.CASCADE)
     hall = models.ForeignKey(Hall, on_delete=models.CASCADE)
+    available_seats = models.IntegerField(validators=[MinValueValidator(0)])
     ticket_cost = models.PositiveIntegerField(default=8)
     cashier = models.ForeignKey(Cashier, on_delete=models.CASCADE)
     date = models.DateTimeField()
@@ -130,6 +127,19 @@ class Session(models.Model):
                f"Date: {self.date}"
 
 
+class Client(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField()
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.first_name}  {self.last_name}"
+
+
 class Purchase(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
     session = models.ForeignKey(Session, on_delete=models.PROTECT)
     bought_at = models.DateTimeField(auto_now_add=True, help_text='Date and time when ticket was bought')
+    updated_at = models.DateTimeField(help_text='Date and time when ticket was updated')
