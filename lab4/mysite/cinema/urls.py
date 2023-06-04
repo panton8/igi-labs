48f43a772a4ce3734a5f3b5aab6d893909ee3ac7
Anton Padvalnikau <podvalnikov0@gmail.com>
2023-06-05 02:07:19 +0300

    Update cinema urls

--- lab4/mysite/cinema/urls.py
+++ lab4/mysite/cinema/urls.py
@@ -3,12 +3,12 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index, name='home'),
-    path('film/<int:film_id>/', show_film, name='film'),
+    path('', FilmHome.as_view(), name='home'),
+    path('film/<int:film_id>/', ShowFilm.as_view(), name='film'),
     path('about/', about, name='about'),
     path('halls/', halls, name='halls'),
     path('emploeeys/', cashiers, name='cashiers'),
     path('buyticket/', buy_ticket, name='buy_ticket'),
     path('login/', login, name='login'),
-    path('genre/<int:genre_id>/', show_genre, name='genre')
+    path('genre/<int:genre_id>/', FilmGenre.as_view(), name='genre')
 ]
