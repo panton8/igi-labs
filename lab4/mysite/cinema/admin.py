48c0a7467deaf6f1e7861ac8b15e67f6ff642a8a
Anton Padvalnikau <podvalnikov0@gmail.com>
2023-06-07 00:33:44 +0300

    Register user

--- lab4/mysite/cinema/admin.py
+++ lab4/mysite/cinema/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from .models import *
+from django.contrib.auth import get_user_model
 
 # Register your models here.
 admin.site.register(Country)
 admin.site.register(Genre)
+User = get_user_model()
+admin.site.register(User)
+
 
 
 @admin.register(Film)
